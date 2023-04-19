214f25bbbaf72778c5ce8e41227b259f4fe4dd38
Ansar <abdykadyrovich.02@gmail.com>
2023-04-19 20:51:32 +0600

    upgrated pager

--- src/components/Pager/Pager.tsx
+++ src/components/Pager/Pager.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import './Pager.scss';
 
 const Pager = () => {
   const navigate = useNavigate();
+
   return (
     <div className="pager">
-      <button onClick={() => navigate(-1)}> &laquo; </button>
-      <Link className='active' to="/"> 1 </Link>
-      <Link className='active' to="/lesson-2"> 2 </Link>
-      <Link className='active' to="/lesson-3"> 3 </Link>
-      <Link className='active' to="/lesson-4"> 4 </Link>
-      <Link className='active' to="/lesson-5"> 5 </Link>
-      <Link className='active' to="/lesson-6"> 6 </Link>
-      <button onClick={() => navigate(1)}> &raquo; </button>
+      <button onClick={() => navigate(-1)}> &laquo; Prev </button>
+      <NavLink className={({ isActive }) => (isActive ? 'active' : 'notActive')} to="/">
+        1
+      </NavLink>
+      <NavLink className={({ isActive }) => (isActive ? 'active' : 'notActive')} to="/lesson-2">
+        2
+      </NavLink>
+      <NavLink className={({ isActive }) => (isActive ? 'active' : 'notActive')} to="/lesson-3">
+        3
+      </NavLink>
+      <NavLink className={({ isActive }) => (isActive ? 'active' : 'notActive')} to="/lesson-4">
+        4
+      </NavLink>
+      <NavLink className={({ isActive }) => (isActive ? 'active' : 'notActive')} to="/lesson-5">
+        5
+      </NavLink>
+      <NavLink className={({ isActive }) => (isActive ? 'active' : 'notActive')} to="/lesson-6">
+        6
+      </NavLink>
+      <button onClick={() => navigate(1)}> Next &raquo; </button>
     </div>
   );
 };
